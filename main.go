45509cddd0b51dfcce582525ecbcbf423f7fdad7
“sunarto” <sunarto.narnia@gmail.com>
2024-08-15 19:55:17 +0700

    create struct model for customer, tenor, and transactions

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := r.Run()
+
+	if err != nil {
+		log.Fatal("Server error")
+	} // listen and serve on 0.0.0.0:8080
+}
